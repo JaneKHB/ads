6cd8205f15da356ab727cea69f20cd4cac492c69
shlee <lee.sangho2@kr.canon>
2024-03-21 08:16:58 +0900

    modify logger

--- service/logger/logger_service.py
+++ service/logger/logger_service.py
@@ -1,8 +1,10 @@
 import logging.handlers
 import os
-import config.app_config as config
 
-class Setting:
+import config.app_config as config
+import util.time_util as time_util
+
+class SettingMain:
     """로거 세팅 클래스
         ::
             Setting.LEVEL = logging.INFO # INFO 이상만 로그를 작성
@@ -13,8 +15,35 @@ class Setting:
     BACKUP_COUNT = config.FILE_LOG_BACKUPCOUNT
     FORMAT = config.FILE_LOG_FORMAT
 
+class SettingLiplusDownload:
+    LEVEL = logging.INFO
+    FILENAME = os.path.join(config.FILE_LOG_FILEPATH, config.FILE_LOG_LIPLUS_FILEPATH, config.FILE_LOG_LIPLUS_DOWNLOAD_FILENAME)
+    MAX_BYTES = config.FILE_LOG_MAXBYTE
+    BACKUP_COUNT = config.FILE_LOG_BACKUPCOUNT
+    FORMAT = config.FILE_LOG_FORMAT
 
-def Logger(name):
+class SettingLiplusUpload:
+    LEVEL = logging.INFO
+    FILENAME = os.path.join(config.FILE_LOG_FILEPATH, config.FILE_LOG_LIPLUS_FILEPATH, config.FILE_LOG_LIPLUS_UPLOAD_FILENAME)
+    MAX_BYTES = config.FILE_LOG_MAXBYTE
+    BACKUP_COUNT = config.FILE_LOG_BACKUPCOUNT
+    FORMAT = config.FILE_LOG_FORMAT
+
+class SettingLiplusGet:
+    LEVEL = logging.INFO
+    FILENAME = os.path.join(config.FILE_LOG_FILEPATH, config.FILE_LOG_LIPLUS_FILEPATH, config.FILE_LOG_LIPLUS_GET_FILENAME)
+    MAX_BYTES = config.FILE_LOG_MAXBYTE
+    BACKUP_COUNT = config.FILE_LOG_BACKUPCOUNT
+    FORMAT = config.FILE_LOG_FORMAT
+
+class SettingLiplusTransfer:
+    LEVEL = logging.INFO
+    FILENAME = os.path.join(config.FILE_LOG_FILEPATH, config.FILE_LOG_LIPLUS_FILEPATH, config.FILE_LOG_LIPLUS_TRANSFER_FILENAME)
+    MAX_BYTES = config.FILE_LOG_MAXBYTE
+    BACKUP_COUNT = config.FILE_LOG_BACKUPCOUNT
+    FORMAT = config.FILE_LOG_FORMAT
+
+def Logger(name, setting):
     """파일 로그 클래스
         :param name: 로그 이름
         :type name: str
@@ -26,7 +55,8 @@ def Logger(name):
 
     # 로거 & 포매터 & 핸들러 생성
     logger = logging.getLogger(name)
-    formatter = logging.Formatter(Setting.FORMAT)
+    formatter = logging.Formatter(setting.FORMAT)
+    formatter.default_msec_format = time_util.TIME_FORMAT_1
     # streamHandler = logging.StreamHandler()
     ### when param ###
     # second - s
@@ -38,8 +68,8 @@ def Logger(name):
     rotatingHandler = logging.handlers.TimedRotatingFileHandler(
         when="d",
         interval=1,
-        filename=Setting.FILENAME,
-        backupCount=Setting.BACKUP_COUNT)
+        filename=setting.FILENAME,
+        backupCount=setting.BACKUP_COUNT)
     rotatingHandler.suffix = config.FILE_LOG_DATEFMT
 
     # 핸들러 & 포매터 결합
@@ -51,6 +81,6 @@ def Logger(name):
     logger.addHandler(rotatingHandler)
 
     # 로거 레벨 설정
-    logger.setLevel(Setting.LEVEL)
+    logger.setLevel(setting.LEVEL)
 
     return logger
